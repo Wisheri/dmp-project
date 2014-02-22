9e6a89e5e412a5de9735b82b977244b7c0b8d925
Atte Sepp„l„ <atte.seppala@gmail.com>
2014-02-22 20:35:42 +0200

    very simple note parsing, added jquery

--- scene.js
+++ scene.js
@@ -1,4 +1,60 @@
-var scene = new THREE.Scene();
+var notes = new Array();
+
+function Note(label, start, end) {
+	this.label = label;
+	this.start = start;
+	this.end = end;
+}
+
+function parseNotes(noteStr) {
+	var noteLen = 100;
+	var currentTime = 0;
+	for (var i=0;i<noteStr.length;i++) 
+	{
+		var currentChar = noteStr.charAt(i);
+		if ( !(currentChar in [' ', '|', ':', '[', ']']) ) 
+		{
+			notes.push(new Note(currentChar, currentTime, currentTime + noteLen));
+		}
+		currentTime += noteLen;
+	}
+}
+
+function readSingleFile(evt) {
+	//Retrieve the first (and only!) File from the FileList object
+	var f = evt.target.files[0]; 
+
+	if (f) {
+	  var r = new FileReader();
+	  r.onload = function(e) { 
+		var contents = e.target.result;
+		var lines = contents.split(/\r\n|\r|\n/); 
+		alert( "Got the file\n" 
+			  + "name: " + f.name + "\n"
+			  + "type: " + f.type + "\n"
+			  + "size: " + f.size + " bytes\n"
+			  + "starts with: " + lines[0]
+		);
+		parseNotes(contents);
+	  }
+	  r.readAsText(f);
+	} else { 
+	  alert("Failed to load file");
+	}
+}
+document.getElementById('fileinput').addEventListener('change', readSingleFile, false);
+
+/*/var oRequest = new XMLHttpRequest();
+var sURL = "\testi.txt";
+
+oRequest.open("GET",sURL,false);
+oRequest.setRequestHeader("User-Agent",navigator.userAgent);
+oRequest.send(null);
+if (oRequest.status==200) alert(oRequest.responseText);
+else alert("Error executing XMLHttpRequest call!");/*/
+
+
+/*/var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
@@ -23,3 +79,4 @@ var render = function () {
 };
 
 render();
+/*/
